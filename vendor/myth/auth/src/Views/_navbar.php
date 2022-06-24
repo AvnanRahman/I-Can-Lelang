b510b635e39f6cc934f1da288df83ecc12078710
HiroomiNan <avnanrahman@gmail.com>
2022-06-24 16:01:59 +0700

    Upload Projek

---
+++ vendor/myth/auth/src/Views/_navbar.php
@@ -0,0 +1,37 @@
+<nav class="navbar navbar-expand-md  fixed-top" style="background-color: #50CEF5">
+    <!-- <a class="navbar-brand" href="#">I-Can Lelang</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button> -->
+    <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <a href="dashboard.html" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-decoration-none">
+            <img class="bi me-2" src="/img/ikon.png" alt="Bootstrap" width="50" height="42">
+            <span class="fs-4 ml-3" style="color: #12106C; font-weight: 600;">I-Can Lelang</span>
+        </a>
+
+
+
+    </div>
+
+    <!-- <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#"><?= lang('Auth.home') ?> <span class="sr-only">(<?= lang('Auth.current') ?>)</span></a>
+            </li>
+        </ul>
+    </div> -->
+</nav>
+
+<!-- <div class="px-3 py-2" style="background-color: #50CEF5">
+    <div class="container">
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <a href="dashboard.html" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-decoration-none">
+                <img class="bi me-2" src="/img/ikon.png" alt="Bootstrap" width="50" height="42">
+                <span class="fs-4" style="color: #12106C; font-weight: 600;">I-Can Lelang</span>
+            </a>
+
+
+
+        </div>
+    </div>
+</div> -->
